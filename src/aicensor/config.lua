3da3ed18654f1106fc5d2759f1b71a5cd5ea98d6
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-01-22 15:48:53 +0100

    Publish FiveM AI.Censor support script

---
+++ src/aicensor/config.lua
@@ -0,0 +1,22 @@
+----------------------------------------------------------------
+--                                                            --
+--    OK! AI.Censor FiveM                                     --
+--                                                            --
+--    Nie wiesz jak skonfigurować? Zerknij do dokumentacji!   --
+--    https://manager.okaeri.eu/aicensor/docs#fivem           --
+--                                                            --
+----------------------------------------------------------------
+
+AiCensorConfig = {
+    --- Klucz prywatny API
+    Token = "",
+    --- Czy filtrować nick pod kątem wulgaryzmów.
+    FilterUsernames = false,
+    --- Czy filtrować wiadomości pod kątem wulgaryzmów.
+    FilterMessages = true
+}
+
+--- Wiadomość
+AiCensorMessages = {
+    Blocked = "Twoja wiadomosc zawierala niedozwolone slowa i zostala zablokowana."
+}
---
+++ src/aicensor/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+server_scripts {
+    'config.lua',
+    'aicensor.lua'
+}
+
+dependencies {
+    'chat'
+}
